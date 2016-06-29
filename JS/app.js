1148e6efa2606633f8dc5eff1c63cfacd6e29362
angiecrellin <angiecrellin@gmail.com>
2016-06-29 10:17:34 -0500

    JS/app.js

--- JS/app.js
+++ JS/app.js
@@ -13,26 +13,46 @@ $(document).ready(function() {
 
 	//generate template for new item
 	var newitem = '<li><input type="checkbox" id="Milk"> <label for="Milk">' + value + '</label> <i class="fa fa-ban"></i></li>'
+	  
 
 	//add it to the list
 
 	$('.listitems').append(newitem);
 
+	
+	
+
 });
+
+	// remove from form after submit
+	
+
+	$('form').on('submit', function(){
+    $('#item').val('');
+
+
+
+})
+
 
 
 //removing an item
 
 //add event listener for clicking on the delete icon
+	$('.fa-ban').on('click', function() {
+
+
 	
 
 //remove that item from the list
-	
+	$(this).closest('li').remove();
+
+})
 
 
 
 //subitems
-
+	
 
 });
 
